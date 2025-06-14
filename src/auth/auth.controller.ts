e58aa79e61d1488e3c3b70fd333138762ac95caf
luisgustavo-mm <luisgustavomm.dev@gmail.com>
2025-06-14 04:09:39 -0300

    Commit 3

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignInDto, SignUpDto } from './dtos/auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private authService: AuthService) {}
 
     @Post('signin')
     async signIn(@Body() body: SignInDto) {
-        console.log({ body });
+        await this.authService.signin(body);
+
         return body;
     }
 
     @Post('signup')
     async signUp(@Body() body: SignUpDto) {
-        console.log({ body });
+        await this.authService.signup(body);
+
         return body;
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { SignInDto, SignUpDto } from './dtos/auth'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async signup(data: SignUpDto) {
+        console.log({ data });
+
+        return 'signup';
+    }
+
+    async signin(data: SignInDto) {
+        console.log({ data });
+
+        return 'signin';
+    }
+}
